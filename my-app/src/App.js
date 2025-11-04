0a0750416e003223fa35bb34b1f1db3d2783fb20
unknown <stanwong86@gmail.com>
2025-11-03 21:19:19 -0500

    Some temporary website deconstruction project that doesn't work

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import About from './components/about';
+import Home from './components/home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
